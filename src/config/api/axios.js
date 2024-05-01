1de1cbe744cecc6bb1c4b074b8485215802b7d27
JatinAhuja-1 <114687972+JatinAhuja-1@users.noreply.github.com>
2024-05-02 00:04:40 +0530

    Update axios.js

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://6d0d-2409-40d1-8d-93a3-68d8-a694-3ada-e1ec.ngrok-free.app/",
+    "https://951e-2409-40d1-6-a785-195d-b96b-821b-1779.ngrok-free.app/",
     // "http://localhost:3500",
 
   headers: { "Content-Type": "application/json" },
