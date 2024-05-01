0da2356f024632c9cb424b9a7d0c22b9806e695e
JatinAhuja-1 <114687972+JatinAhuja-1@users.noreply.github.com>
2024-05-01 19:51:29 +0530

    Update axios.js

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://kollege-api.onrender.com",
+    "https://6d0d-2409-40d1-8d-93a3-68d8-a694-3ada-e1ec.ngrok-free.app/",
     // "http://localhost:3500",
 
   headers: { "Content-Type": "application/json" },
